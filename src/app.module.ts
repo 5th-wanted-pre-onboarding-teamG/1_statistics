250041ff091726434aeb77ed3e80cb87d9a820b4
Seongtae <stae1102@gmail.com>
2022-10-26 10:32:44 +0900

    Chore: 추가 typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mysql',
+          host: 'localhost',
+          port: 3306,
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          migrations: [__dirname + '/src/migrations/*.ts'],
+          autoLoadEntities: true,
+          synchronize: false,
+          logging: true,
+          keepConnectionAlive: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
