e0688cf5a766aa58309b53a2fafe006bf07d8b5d
MisterRuby <ruby8700@naver.com>
2022-10-26 20:05:54 +0900

    feat: 세션 설정 config 추가

---
+++ src/auth/auth.session.config.ts
@@ -0,0 +1,17 @@
+import * as session from "express-session"
+import * as passport from "passport"
+
+export const sessionConfig = (app) => {
+    app.use(session({
+        name: "NEST_SESSION_ID",
+        resave: false,
+        saveUninitialized: true,
+        secret: process.env.COOKIE_SECRET,
+        cookie: {
+            maxAge: 60000,
+        }
+    }));
+
+    app.use(passport.initialize())
+    app.use(passport.session())
+}
