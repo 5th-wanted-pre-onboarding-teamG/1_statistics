9eda625f6d01e70dd14fbd7afc7cb6ff7e1a40da
MisterRuby <ruby8700@naver.com>
2022-10-27 16:22:15 +0900

    comment: auth controller 주석 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,13 +3,21 @@ import { AuthenticatedGuard, AuthLocalGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  /**
+   * @url POST '/auth/login'
+   * @description 로그인 성공/실패 여부를 반환합니다.
+   */
   @UseGuards(AuthLocalGuard)
   @Post('login')
-  async login() {}
+  login(): void {}
 
+  /**
+   * @url GET '/auth/logout'
+   * @description 로그아웃 처리를 위해 세션을 제거합니다.
+   */
   @UseGuards(AuthenticatedGuard)
   @Get('logout')
-  async logout(@Req() req) {
+  logout(@Req() req): void {
     req.session.destroy();
   }
 }
