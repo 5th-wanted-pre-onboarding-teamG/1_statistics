6ae47e5e1d14c4113819f834940cdbcad83a9198
eunkangchoi <ekk12mv2@gmail.com>
2022-10-27 10:11:05 +0900

    fix: 수정 bcrypt 오류해결

--- src/auth/auth.strategy.ts
+++ src/auth/auth.strategy.ts
@@ -7,7 +7,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { Repository } from 'typeorm';
 import { Users } from '../entities/Users';
-import { bcrypt } from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
