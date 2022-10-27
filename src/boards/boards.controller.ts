6ec6b45dddba5366b4e2a067dff2898536865062
Seongtae <stae1102@gmail.com>
2022-10-27 14:06:47 +0900

    comment: 수정, 추가 boards controller 주석
    boards controller 주석에 일치하지 않는 내용 변경
    - getBoardsByKind에서 param -> query로 변경
    - getSpecificBoard에서 api 엔드포인트를 수정
    내용 추가
    - GET /boards/search?kind=query 주석 추가
    
    Resolves: #31

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -8,7 +8,8 @@ export class BoardsController {
 
   /**
    * 종류에 따라 게시물을 조회합니다.
-   * @param kind 게시물 종류
+   * @api GET /boards/search?kind=kind
+   * @query kind 게시물 종류 { '자유' | '운영' | '공지' }
    * @returns 종류에 맞는 게시물을 객체 배열로 반환합니다.
    */
   @Get('search')
@@ -18,7 +19,7 @@ export class BoardsController {
 
   /**
    * 게시물 아이디로 특정 게시물을 조회합니다.
-   * @api GET /boards/:boardId
+   * @api GET /boards/search/:boardId
    * @param boardId 게시물 아이디
    * @returns 특정 게시물 아이디의 게시물 한 개를 반환합니다.
    */
