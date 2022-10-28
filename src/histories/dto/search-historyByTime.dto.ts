51c3bbe72b68670ac75baefd181ff2f956283baf
MisterRuby <ruby8700@naver.com>
2022-10-28 19:27:21 +0900

    feat: 접속 이력 시간별 조회 Dto Validation 추가

--- src/histories/dto/search-historyByTime.dto.ts
+++ src/histories/dto/search-historyByTime.dto.ts
@@ -1,4 +1,10 @@
+import { IsDateString, IsNotEmpty } from 'class-validator';
+
 export class SearchHistoryByTimeDto {
+  @IsNotEmpty()
+  @IsDateString()
   startDate: Date;
+  @IsNotEmpty()
+  @IsDateString()
   endDate: Date;
 }
