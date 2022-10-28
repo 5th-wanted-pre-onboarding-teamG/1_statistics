4f93398bbc264908e17a834f6671396c0375a365
Seongtae <stae1102@gmail.com>
2022-10-28 10:44:06 +0900

    chore: 제거 users module import
    불필요한 임포트 제거
    
    Resolves: #31

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,7 +3,6 @@ import { BoardsService } from './boards.service';
 import { BoardsController } from './boards.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Boards } from 'src/entities/Boards';
-import { UsersModule } from 'src/users/users.module';
 
 @Module({
   // TypeOrmModule에서 게시물 에티티를 임포트합니다.
