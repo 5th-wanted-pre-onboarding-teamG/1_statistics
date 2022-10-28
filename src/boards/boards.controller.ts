9eaf1597a289a5ad0a81cabedb6d9cffd74aa8d4
MisterRuby <ruby8700@naver.com>
2022-10-28 20:19:37 +0900

    refactor: 게시글 상세조회 url 의 search 단어 제거
    HTTP GET 메소드 자체가 조회를 의미하기 때문에 search 단어로 표시할 필요가 없음

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -37,7 +37,7 @@ export class BoardsController {
    * @param boardId 게시물 아이디
    * @returns 특정 게시물 아이디의 게시물 한 개를 반환합니다.
    */
-  @Get('search/:boardId')
+  @Get(':boardId')
   // ParseIntPipe로 string으로 들어오는 param값을 number로 바꿉니다.
   getSpecificBoard(@Param('boardId', ParseIntPipe) boardId: number) {
     return this.boardsService.getSpecificBoard(boardId);
