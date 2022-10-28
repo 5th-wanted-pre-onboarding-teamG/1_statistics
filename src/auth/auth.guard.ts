04860956462d12242afc336c0219214ce7feda49
MisterRuby <ruby8700@naver.com>
2022-10-28 18:02:18 +0900

    style: Guard 인증 코드 정리

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,9 +1,4 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserRank } from '../entities/enums/userRank';
 import { Users } from '../entities/Users';
@@ -27,14 +22,7 @@ export class AuthenticatedGuard implements CanActivate {
    */
   async canActivate(context: ExecutionContext): Promise<any> {
     const request = context.switchToHttp().getRequest();
-    const user = request?.session?.passport?.user;
-
-    // 세션에 저장되어 있는 유저 정보 반환
-    if (user) {
-      return user;
-    }
-
-    throw new UnauthorizedException();
+    return request?.session?.passport?.user;
   }
 }
 
@@ -49,22 +37,15 @@ export class OperateGuard implements CanActivate {
     const request = context.switchToHttp().getRequest();
     const user = request?.session?.passport?.user;
 
-    // 세션에 저장되어 있는 유저 정보 반환
-    if (this.isOperateRank(user)) {
-      return user;
-    }
-
-    // 사용자 정보가 세션에 없거나 권한이 없을 시 에러 처리
-    throw new UnauthorizedException();
+    return this.isOperateRank(user);
   }
 
   /**
    * 유저의 권한이 운영자 또는 관리자인지 확인
    * @param user
-   * @
+   * @return 유저의 권한이 운영자 또는 관리자인지 판별값
    */
   isOperateRank(user: Users): boolean {
-    console.log(user.rank);
     return (
       user && (user.rank === UserRank.MANAGER || user.rank === UserRank.ADMIN)
     );
