7a213c7fd3f730ebef70571efe75a72d1e5ec12b
Seongtae <stae1102@gmail.com>
2022-10-28 15:52:21 +0900

    perf: 추가 create-board-dto 검사
    게시물 생성에 전송되는 body값을 검사할 때
    빈 값을 받아와 데이터베이스에 저장하면 안되므로
    유효성검사를 해줍니다.
    
    Fixes: #42

--- src/boards/dto/create-board.dto.ts
+++ src/boards/dto/create-board.dto.ts
@@ -1,6 +1,12 @@
+import { IsNotEmpty, IsString } from 'class-validator';
 import { BoardKind } from 'src/entities/enums/boardKind';
 
 export class CreateBoardDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
+  @IsString()
   content: string;
 }
